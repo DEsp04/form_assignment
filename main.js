70482d27729eb4993d6d37be77615c8597fa245d
David Lora <despinal0425@gmail.com>
2022-07-11 21:23:07 -0400

    added form inputs

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import './style.css';
 
-// eslint-disable-next-line no-undef
-document.querySelector('#app').innerHTML = ``;
+// // eslint-disable-next-line no-undef
+// document.querySelector('#app').innerHTML = ``;
