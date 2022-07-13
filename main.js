a391ef9030ee7728b8cfcaa156063424984c8bf7
David Lora <despinal0425@gmail.com>
2022-07-12 22:42:03 -0400

    several getElementsById to retrieve user inputs & submit button

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 import './style.css';
 
-// // eslint-disable-next-line no-undef
 // document.querySelector('#app').innerHTML = ``;
+// eslint-disable-next-line no-undef
+const firstNameInput = document.getElementById('fname');
+// eslint-disable-next-line no-undef
+const submitButtom = document.getElementById('submit');
+
+// eslint-disable-next-line no-undef
+document.getElementById('pets').addEventListener('change', (e) => {
+  const petText = e.target.options[e.target.selectedIndex].text;
+  console.log(petText);
+});
+
+submitButtom.addEventListener('click', () => {
+  console.log(firstNameInput.value);
+});
